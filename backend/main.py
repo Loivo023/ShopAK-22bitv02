8f7ae4275076e8db437a061c04962d0a2a323cbb
Ryu <160713715+BDragon00@users.noreply.github.com>
2026-06-08 21:08:06 +0700

    Update
    Update FE and BE

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,35 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {
-        "message": "Welcome to ShopHub API"
+        "message": "Welcome to ShopAK API"
     }
+PRODUCTS_DATA = [
+    {
+        "id": 1,   
+        "name": "Keyboard",
+    },
+    {
+        "id": 2,
+        "name": "Mouse",
+    },
+    {
+        "id": 3,
+        "name": "Monitor",
+    }
+]
+
+@app.get("/products")
+def get_products():
+    return PRODUCTS_DATA
+
+@app.get("/products/{product_id}")
+def get_product_by_id(product_id: int):
+    for item in PRODUCTS_DATA:
+        if item["id"] == product_id:
+            return item
+    return {"error": "Product not found"}
