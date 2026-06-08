1716b176b69c640c5506d74f73eef310331c7410
Loivo023 <loivo023@gmail.com>
2026-06-08 21:09:04 +0700

    session-1
    session 1

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -7,3 +8,31 @@ def root():
     return {
         "message": "Welcome to ShopAK API"
     }
+
+class Product(BaseModel):
+    id: int
+    name: str
+    price: float
+    description: str | None = None
+
+@app.get("/about")
+def about():
+    return {
+        "project": "ShopAK",
+        "version": "1.0"
+    }
+
+PRODUCT_DATA = [
+    {
+        "id": 1,
+        "name": "laptop"
+    },
+    {
+        "id": 2,
+        "name": "Mouse"
+    }
+]
+
+@app.get("/products")
+def get_all_product():
+    return PRODUCT_DATA
